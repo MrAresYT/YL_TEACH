070c3f4c4706c04e9def295a75e9edaac5d5986b
Дмитрий <dima.egorkin.2015@mail.ru>
2024-03-27 12:48:19 +0300

    Решение задачи

---
+++ Вывод предложений.py
@@ -0,0 +1,29 @@
+class Paragraph:
+    def __init__(self, width: int):
+        self.width = width
+        self.lines = []
+        self.line = ''
+
+    def add_word(self, word):
+        if self.line:
+            if len(self.line + ' ' + word) <= self.width:
+                self.line += ' ' + word
+            else:
+                self.lines.append(self.line)
+                self.line = word
+        else:
+            self.line += word
+
+
+class LeftParagraph(Paragraph):
+    def end(self):
+        print('\n'.join(self.lines))
+
+
+class RightParagraph(Paragraph):
+    def end(self):
+        print('\n'.join(i.rjust(self.width, ' ') for i in self.lines))
+
+
+if __name__ == '__main__':
+    pass
