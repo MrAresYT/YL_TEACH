2e9fea891b434a8961ea3a18e18e7ba50fb31d48
Ares <dima.egorkin.2015@mail.ru>
2024-04-17 18:22:00 +0300

    Задача

--- Шахматы.py
+++ Шахматы.py
@@ -1,4 +1,4 @@
-class Pawn:
+class Figure:
 
     def __init__(self, row, col, color):
         self.row = row
@@ -12,10 +12,34 @@ class Pawn:
     def char(self):
         return self.__class__.__name__[0]
 
+    def get_color(self):
+        return self.color
 
-class Queen(Pawn):
-    pass
+
+class Knight(Figure):
+    def can_move(self, row1, col1):
+        n = row1 + col1 - self.col + self.row
+        if abs(n) == 3:
+            return True
+        else:
+            return False
 
 
-pie = Queen(1, 1, 1)
-print(pie.char())
+
+WHITE = 1
+BLACK = 2
+
+row0 = 2
+col0 = 2
+knight = Knight(row0, col0, WHITE)
+
+print('white' if knight.get_color() == WHITE else 'black')
+for row in range(7, -1, -1):
+    for col in range(8):
+        if row == row0 and col == col0:
+            print(knight.char(), end='')
+        elif knight.can_move(row, col):
+            print('x', end='')
+        else:
+            print('-', end='')
+    print()
